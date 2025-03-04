242219e889817e9a3f29fff89f61cb9f472e8522
iren42 <isabelleren618@gmail.com>
2025-03-04 19:22:35 +0100

    feat: sidebar appereance OK

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-import "./normalize.css";
-import "./style.css";
+import "./css/normalize.css";
+import "./css/style.css";
 
 if (process.env.NODE_ENV !== 'production')
 {
     console.log('Looks like we are in development mode!');
+}
+{
+    let sideBar = document.querySelector('.sidebar');
+    let arrowCollapse = document.querySelector('#collapse-sidebar-icon');
+    sideBar.onclick = () =>
+    {
+        sideBar.classList.toggle('collapse');
+        arrowCollapse.classList.toggle('collapse');
+        if (arrowCollapse.classList.contains('collapse'))
+        {
+            arrowCollapse.classList = "fi fi-rr-angle-right collapse";
+        }
+        else
+        {
+            arrowCollapse.classList = "fi fi-rr-angle-left";
+        }
+    };
 }
