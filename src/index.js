59435fa157e87540da725de89f000e6f6f8c107d
iren42 <isabelleren618@gmail.com>
2025-03-08 21:02:49 +0100

    feat: try catch block

--- src/index.js
+++ src/index.js
@@ -49,11 +49,11 @@ const DOMController = (function ()
         projectListDiv.innerHTML = "";
         editor.innerHTML = "";
         preview.innerHTML = "";
+
         for (let i = 0; i < list.length; i++)
         {
             const key = list.key(i);
             console.log(`key: ${ key }`);
-            // const stored = JSON.parse(list.getItem(key));
             const stored = Storage.getItem(localStorage, key);
             const li = DOMCreator.project(stored.name, stored.id);
             projectListDiv.append(li);
@@ -62,7 +62,7 @@ const DOMController = (function ()
 
     function view(where, what)
     {
-        where.innerText = "";
+        where.innerHTML = "";
         where.append(what);
     }
 
@@ -80,50 +80,55 @@ const DOMController = (function ()
 
     document.addEventListener("click", event =>
     {
-        // console.log(event.target);
-        // event.preventDefault();
-        if (event.target.closest("#addNewProject"))
+        try
         {
-            console.log("add a new project");
-            const newProject = new Project();
+            if (event.target.closest("#addNewProject"))
+            {
+                console.log("add a new project");
+                const newProject = new Project();
 
-            Storage.setItem(localStorage, newProject.id, newProject);
+                Storage.setItem(localStorage, newProject.id, newProject);
 
-            // immediately open that new project on the editor view
-            view(editor, newProject.content);
-            updateScreen(localStorage);
-            const active = document.querySelector(`#${ CSS.escape(newProject.id) }`);
-            active.classList.add("active");
+                // immediately open that new project on the editor view
+                view(editor, newProject.content);
+
+                updateScreen(localStorage);
+                const button = document.querySelector(`#${ CSS.escape(newProject.id) }`);
+                button.classList.add("active");
+            }
+            else if (event.target.closest("#clearBtn"))
+            {
+                console.log("clear data");
+                Storage.clear(localStorage);
+                updateScreen(localStorage);
+            }
+            else if (event.target.closest("#saveBtn"))
+            {
+                console.log("saving");
+                // edit project obj 
+                const key = document.querySelector(".project.active").id;
+                if (!key)
+                    throw new Error(`Could not save, no ID found`);
+                const activeProject = Storage.getItem(localStorage, key);
+                activeProject.content = editor.innerText;
+                Storage.setItem(localStorage, activeProject.id, activeProject);
+            }
+            else if (event.target.closest("button.project"))
+            {
+                let target = findParentElByClass(event.target, "project");
+                if (!(target.classList.contains("active")))
+                    target.classList.add("active");
+                const stored = Storage.getItem(localStorage, target.id);
+                if (!stored)
+                    throw new Error(`no stored item for this key: ${ target.id }`);
+                view(editor, stored.content)
+            }
+            else;
         }
-        else if (event.target.closest("#clearBtn"))
+        catch (e)
         {
-            console.log("clear data");
-            Storage.clear(localStorage);
-            updateScreen(localStorage);
+            console.error("In event listener:", e.message);
         }
-        else if (event.target.closest("#saveBtn"))
-        {
-            console.log("saving");
-            // edit project obj 
-            const key = document.querySelector(".project.active").id;
-            if (!key)
-                return;
-            const activeProject = Storage.getItem(localStorage, key);
-            activeProject.content = editor.innerText;
-            Storage.setItem(localStorage, activeProject.id , activeProject);
-        }
-        else if (event.target.closest("button.project"))
-        {
-            console.log(`view: project `);
-            let target = findParentElByClass(event.target, "project");
-            if (!(target.classList.contains("active")))
-                target.classList.add("active");
-            const stored = Storage.getItem(localStorage, target.id);
-            if (!stored)
-                console.log(`no stored items: ${target.id}`);
-            view(editor, stored.content)
-        }
-        else;
     });
 
     updateScreen(localStorage);
