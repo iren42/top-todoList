11a4d6a91a4ff6cf5d7eab76f3bc18d487759b40
iren42 <isabelleren618@gmail.com>
2025-03-17 14:17:01 +0100

    feat: rename project

--- src/index.js
+++ src/index.js
@@ -42,11 +42,12 @@ editor.addEventListener("input", event =>
 
 const DOMController = (function ()
 {
-    function removeActiveClasses(){
+    function removeActiveClasses()
+    {
         const list = document.querySelectorAll(".project.active");
         if (!list)
-            return ;
-        for(let i = 0; i < list.length; i++)
+            return;
+        for (let i = 0; i < list.length; i++)
         {
             list[i].classList.remove("active");
         }
@@ -88,11 +89,42 @@ const DOMController = (function ()
         return (element);
     }
 
+    function renameProject(element)
+    {
+        let IDElement = findParentElByClass(element, "project");
+        if (!IDElement)
+            throw new Error(`Could not find <button class="project">`);
+        let textEl = IDElement.querySelector(".project-text");
+        if (!textEl)
+            throw new Error(`Could not find class="project-text"`);
+        textEl.contentEditable = true;
+        textEl.focus();
+        textEl.addEventListener("keydown", event =>
+        {
+            if (event.key === "Enter")
+            {
+                event.preventDefault();
+                // update database
+                const stored = Storage.getItem(localStorage, IDElement.id);
+                if (!stored)
+                    throw new Error(`No stored item for this key: ${ IDElement.id }`);
+                stored.name = textEl.innerText;
+                Storage.setItem(localStorage, stored.id, stored);
+                console.log(`renamed project to ${ stored.name }`);
+                textEl.contentEditable = false;
+            }
+        })
+    }
+
     document.addEventListener("click", event =>
     {
         try
         {
-            if (event.target.closest("#addNewProject"))
+            if (event.target.closest(".rename-project"))
+            {
+                renameProject(event.target);
+            }
+            else if (event.target.closest("#addNewProject"))
             {
                 console.log("add a new project");
                 const newProject = new Project();
